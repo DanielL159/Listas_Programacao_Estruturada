14ac487727f343aee7974c01fd01aa9cf1794dbd
DanielL159 <danielluizdossantos12@gmail.com>
2022-10-31 10:40:20 -0300

    Exercicio 5 a terminar

--- Lista_Struct_Estruturada.c
+++ Lista_Struct_Estruturada.c
@@ -144,6 +144,36 @@ int main(){
 
 //exercicio 5 (NAo ententdi o enunciado)
 
+struct dados
+{
+  int a,b,c;
+};
+
+int main (){
+  struct dados vetor[2];
+  int i,resultado[3];
+
+  for ( i = 0; i < 2; i++)
+  {
+    printf("Qual o valor de A:");
+    scanf("%d",vetor[i].a);
+
+    printf("Qual o valor de B:");
+    scanf("%d",vetor[i].b);
+
+    printf("Qual o valor de C:");
+    scanf("%d",vetor[i].c);
+  }
+  resultado[0]=vetor[0].a + vetor[1].a;
+  resultado[1]=vetor[0].b + vetor[1].b;
+  resultado[3]=vetor[0].c + vetor[1].c;
+
+
+}
+
+
+
+
 
 //Exercicio 6
 /*
